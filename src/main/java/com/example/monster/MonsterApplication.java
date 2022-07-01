6c97da4e781f8ef69cd62dd69d27245ac2380661
bavuchoko <bavuchoko@naver.com>
2022-07-01 17:09:11 +0900

    jenkins test

--- src/main/java/com/example/monster/MonsterApplication.java
+++ src/main/java/com/example/monster/MonsterApplication.java
@@ -10,4 +10,5 @@ public class MonsterApplication {
         SpringApplication.run(MonsterApplication.class, args);
     }
 
+
 }
--- src/main/java/com/example/monster/common/SecurityConfig.java
+++ src/main/java/com/example/monster/common/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.monster.common;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +17,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
