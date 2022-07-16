9a2321e4a83fb176743ebb3db20a008ce1a02a98
pjs <bavuchoko@naver.com>
2022-07-16 14:17:09 +0900

    a

--- src/main/java/com/example/monster/MonsterApplication.java
+++ src/main/java/com/example/monster/MonsterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = { "classpath:jdbc.properties" })
 public class MonsterApplication {
 
     public static void main(String[] args) {
