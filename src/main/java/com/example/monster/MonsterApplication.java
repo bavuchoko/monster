e82faa8f6b128cac56a042a43dfd2005710f1b28
pjs <bavuchoko@naver.com>
2022-07-16 13:51:26 +0900

    jdbc setting file seperate

--- src/main/java/com/example/monster/MonsterApplication.java
+++ src/main/java/com/example/monster/MonsterApplication.java
@@ -2,8 +2,10 @@ package com.example.monster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:jdbc.properties" })
 public class MonsterApplication {
 
     public static void main(String[] args) {
