c9df21ed859e04ffddd11e274e158bbcfd63efb8
pjs <bavuchoko@naver.com>
2022-08-06 22:29:51 +0900

    cors

--- src/main/java/com/example/monster/config/security/SecurityConfig.java
+++ src/main/java/com/example/monster/config/security/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("http://pjs.monster");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
