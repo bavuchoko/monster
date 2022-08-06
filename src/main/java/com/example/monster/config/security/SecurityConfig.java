685a8c15da831b6257cd09d844c16fd2adaddc64
pjs <bavuchoko@naver.com>
2022-08-07 00:01:14 +0900

    cors

--- src/main/java/com/example/monster/config/security/SecurityConfig.java
+++ src/main/java/com/example/monster/config/security/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
@@ -71,7 +72,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
@@ -101,6 +102,7 @@ public class SecurityConfig {
 
                 .and()
                     .authorizeRequests()
+                    .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                     .antMatchers("/api/**").permitAll()
                     .antMatchers("/admin/*").hasRole("ADMIN")
                     .anyRequest().authenticated()
