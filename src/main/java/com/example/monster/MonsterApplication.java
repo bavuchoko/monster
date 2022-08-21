57f47c4ba3e144bbca4783699e5e2d3f9997c1cc
pjs <bavuchoko@naver.com>
2022-08-21 20:03:33 +0900

    a

--- src/main/java/com/example/monster/MonsterApplication.java
+++ src/main/java/com/example/monster/MonsterApplication.java
@@ -1,9 +1,13 @@
 package com.example.monster;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
@@ -13,6 +17,8 @@ public class MonsterApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonsterApplication.class, args);
     }
+
+
 
 
 }
